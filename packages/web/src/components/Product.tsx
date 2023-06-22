e9f3ea598e0220415fafbebc40db4e4a0015f356
Toni Moreira <toni@itentecnologia.com.br>
2023-06-22 00:17:16 -0300

    Remove default image

--- packages/web/src/components/Product.tsx
+++ packages/web/src/components/Product.tsx
@@ -2,7 +2,7 @@ function Product({product, tax}: {product: any, tax?: any})
 {
   const image = product.images[0]?.image
   return <div className="border p-4 rounded-lg">
-    <img className="h-24" src={image ? 'https://www.megaeletronicos.com:4420/img/'+ image.replace('/uploads/Product/', '').replaceAll('/', '-') : '/logo.jpeg'} />
+    <img className="h-24" src={image && !image.includes('default') ? 'https://www.megaeletronicos.com:4420/img/'+ image.replace('/uploads/Product/', '').replaceAll('/', '-') : '/logo.jpeg'} />
     <h2 className="font-light mt-4 text-neutral-600">{product.category?.name}</h2>
     <h1 className="font-semibold">{product.title}</h1>
     {!!product.cost && <div className="flex items-center justify-items-start gap-x-1">
